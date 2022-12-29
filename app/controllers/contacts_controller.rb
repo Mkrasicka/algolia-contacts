65dbe25d3f56ea09bf02d3fc7ef20b05630f4150
magdalena krasicka <krasickamagdalena@gmail.com>
2022-12-29 03:04:51 +0000

    add dotenv-rails

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,9 +2,13 @@ class ContactsController < ApplicationController
   def index
     if params[:query].present?
       @contacts = Contact
-                  .search("lower(firstname) LIKE :query OR lower(lastname) LIKE :query", query: "%#{params[:query]}%")
+                  .search("lower(firstname) LIKE :query
+                  OR lower(lastname) LIKE :query",
+                  query: "%#{params[:query]}%")
     else
       @contacts = Contact.all
     end
+
+    @hits = Contact.search(@q, { hitsPerPage: 5, page: (params['page'] || 1) })
   end
 end
